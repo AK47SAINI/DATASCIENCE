eb0d2b32b1c68b862004baca23e617919d75f751
AK47SAINI <aksaini2282003@gmail.com>
2024-05-27 13:22:16 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,30 @@
+a="aashish"
+b=" kumar saini"
+#concatenation of the strings
+
+print(a+b)
+print(''.join([a,b]))
+
+
+#to print the lowercase of the string
+print(a.lower())
+
+
+#for printing the length of the string
+print(len(b))
+
+c="   helloLIET   "
+  
+
+  #for removing the spaces from the left of the string
+print(c.lstrip())
+
+
+
+
+l1=["aashish","kumar","saini"]
+# In insert function we have to give index at which we want to insert value
+l1.insert(0,"hello ")
+#append function append the value in the end of the list
+l1.append(" By")
+print(l1)
