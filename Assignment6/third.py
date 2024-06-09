32e891efc935f36c336acc78b96d49bcd410e2fb
AK47SAINI <aksaini2282003@gmail.com>
2024-06-09 14:31:01 +0530

    first comment

--- Assignment6/third.py
+++ Assignment6/third.py
@@ -1,4 +1,8 @@
 import numpy as np
 a=np.arange(1,61,1).reshape(3,4,5)
 print(a)
-print(a[1:,-2::1,])
+print("The last 2 rows of the last 2 matrix are as follows:")
+
+print(a[1:,-2::1,:])
+print("The last 2 columns of the last 2 matrices are as follows:")
+print(a[1:,:,-2::1])
